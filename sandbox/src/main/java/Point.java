74c0b4b83b506f46cbf64a957de6bfe6a12a00ad
LinaB25 <amilinka@yandex.ru>
2022-12-10 11:35:47 +0300

    Задание №2

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,10 @@
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point(double x1, double y1) {
+        this.x = x1;
+        this.y = y1;
+    }
+}
--- sandbox/src/main/java/Prog.java
+++ sandbox/src/main/java/Prog.java
@@ -1,7 +1,14 @@
 public class Prog {
 
- public static void main(String[] args) {
-System.out.println("Hello, world");
-}
+    public static void main(String[] args) {
+        Point p1 = new Point(3, 10);
+        Point p2 = new Point(30, 31);
+        System.out.println("Расстояние между точками = " + distance(p1, p2));
+    }
 
+    public static double distance(Point p1, Point p2) {
+        double a = Math.pow((p1.x - p2.x), 2);
+        double b = Math.pow((p1.y - p2.y), 2);
+        return Math.sqrt((a + b));
+    }
 }
