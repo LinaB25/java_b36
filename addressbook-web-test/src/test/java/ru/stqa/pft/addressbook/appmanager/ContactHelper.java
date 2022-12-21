ff5aad1250ddd41f9e26ed78cd4e01c6ce90c142
LinaB25 <amilinka@yandex.ru>
2022-12-21 19:09:53 +0600

    Рефактор ContactHelper + тест ContactModificationTests

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -12,37 +12,33 @@ import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
-public class ContactHelper {
-    private WebDriver wd;
+public class ContactHelper extends HelperBase {
     public ContactHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void submit() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        click(By.xpath("//div[@id='content']/form/input[21]"));
     }
 
     public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("theform")).click();
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        type(By.name("firstname"), contactData.getFirstName());
+        type(By.name("lastname"), contactData.getLastName());
+        type(By.name("address"), contactData.getAddress());
+        type(By.name("mobile"), contactData.getMobile());
+        type(By.name("email"), contactData.getEmail());
     }
 
     public void createContact() {
-        wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
+    }
+
+    public void initContactModification() {
+        click(By.id("1"));
+        click(By.xpath("//img[@alt='Edit']"));
+    }
+
+    public void submitModificationContact() {
+        click(By.xpath("//div[@id='content']/form/input[22]"));
     }
 }
---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase{
+    @Test
+    public void testContactModification(){
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("Natasha", "Petrova", "Moscow", "89745684411", "test@testmail.ru"));
+        app.getContactHelper().submitModificationContact();
+        app.getNavigationHelper().gotoHomePage();
+    }
+}
