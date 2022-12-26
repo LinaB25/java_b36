6592e525d69c1cc157e3a11ba791209e02c91688
LinaB25 <amilinka@yandex.ru>
2022-12-26 09:21:00 +0600

    Лекция 3.5

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,10 +6,10 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupCreationTests extends TestBase {
 
     @Test
-    public void testCreationGroup() throws Exception {
+    public void testCreationGroup() {
         app.getNavigationHelper().gotoPageGroup();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
