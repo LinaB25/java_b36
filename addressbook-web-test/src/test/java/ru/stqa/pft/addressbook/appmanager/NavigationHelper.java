394713c10df8ff2ad2ee9f5f5654975925fab7fa
LinaB25 <amilinka@yandex.ru>
2022-12-26 10:11:57 +0600

    Лекция 3,9

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,17 +3,27 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
     public void gotoPageGroup() {
+        if (isElementPresented(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresented(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
+
     }
 
-    public void gotoHomePage(){
+    public void gotoHomePage() {
+        if (isElementPresented(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home page"));
     }
+
 }
