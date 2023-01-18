bd1e188598396275aefab7b24eb9152f53f94bff
LinaB25 <amilinka@yandex.ru>
2023-01-18 13:14:09 +0600

    Задание 14

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,37 +7,46 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.Browser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private NavigationHelper navigationHelper;
+    private final Properties properties;
     WebDriver wd;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser) throws IOException {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if(Objects.equals(browser, Browser.FIREFOX.browserName())){
             wd = new FirefoxDriver(new FirefoxOptions().setBinary("c:\\Program Files\\Mozilla Firefox\\firefox.exe"));
         } else if (Objects.equals(browser, Browser.CHROME.browserName())){
             wd = new ChromeDriver();
-        } else if (Objects.equals(browser, Browser.IE.browserName())) {
+        } else if (Objects.equals(browser, Browser.IE.browserName())){
             wd = new InternetExplorerDriver();
         }
-
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
