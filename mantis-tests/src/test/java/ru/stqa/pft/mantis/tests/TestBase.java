7f76c8e0dd51886e67bb9e35b6e8632497a9bb60
LinaB25 <amilinka@yandex.ru>
2023-02-08 12:15:51 +0600

    19

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,20 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import org.apache.axis.types.Id;
 import org.openqa.selenium.remote.Browser;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -20,16 +28,32 @@ public class TestBase {
         }
     }
 
-   @BeforeSuite(alwaysRun = true)
+    @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
         app.init();
-        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config/config_inc.php", "config_inc.php.bak");
+       // app.ftp().upload(new File("src/test/resources/config_inc.php"), "config/config_inc.php", "config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws IOException {
-        app.ftp().restore("config_inc.php.bak", "config/config_inc.php");
+     //   app.ftp().restore("config_inc.php.bak", "config/config_inc.php");
         app.stop();
     }
 
+    public boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException, ServiceException {
+        MantisConnectPortType mc = app.soap().getMantisConnect();
+        IssueData bug = mc.mc_issue_get("administrator","root", BigInteger.valueOf(issueId));
+        String issueStatus = bug.getStatus().getName();
+        if(issueStatus.equals("resolved")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
 }
